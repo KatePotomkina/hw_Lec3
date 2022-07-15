376194910ce8a523ba26d75e16a225bc4528bbc1
kate.potomkina <kate.potomkina@gmail.com>
2022-07-15 14:59:23 +0300

    Добавьте файлы проекта.

---
+++ hw_Lec3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace hw_Lec3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            factorial(5);
+            fibonacci(50);
+                               }
+        public static void factorial(int num)
+        {
+            IEnumerable<int> query = Enumerable.Range(1, num);
+            int factorial = query.Aggregate((a, b) => a * b);
+            Console.WriteLine(factorial.ToString());
+        }
+        public static void fibonacci(int num)
+        {
+            List<int> query1 = new List<int>();
+            Enumerable.Range(0, 200)
+                 .ToList()
+                 .ForEach(i =>
+                     query1.Add(i <= 0 ? 0:
+                         i <= 1 ? 1 : query1[i - 2] + query1[i - 1])) ;
+            for (int k = 0; k < num; k++)
+            {
+                Console.Write(query1[k].ToString()+" ");
+            }
+        }
+    }
+}
